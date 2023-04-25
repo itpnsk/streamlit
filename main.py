0118bad68671ea7d2ad2b380783395354abb947c
Andrej Oravec <andrej@oravec.sk>
2023-04-25 12:21:18 +0200

    added number 2

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ st.title('Uber pickups in USA')
 st.title(random.randint(1,100))
 
 number1 = st.number_input('Insert a number')
-# number2 = st.number_input('Insert a number')
+number2 = st.number_input('Insert a number')
 
 # st.write('The current number is ', number1 + number2)
