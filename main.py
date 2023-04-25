3c4e7bfdade3878ff507142e6ee11ec9e9d1db4c
Andrej Oravec <andrej@oravec.sk>
2023-04-25 12:25:11 +0200

    added keys

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 st.title('Uber pickups in USA')
 st.title(random.randint(1,100))
 
-jozo = st.number_input('Insert a number')
-fero = st.number_input('Insert a number')
+number1 = st.number_input('Insert a number', key=1)
+number2 = st.number_input('Insert a number', key=2)
 
-# st.write('The current number is ', number1 + number2)
+st.write('The current number is ', number1 + number2)
