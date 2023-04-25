8d5022a619892975589d5bf5822557495dd7ca3a
Andrej Oravec <andrej@oravec.sk>
2023-04-25 12:22:37 +0200

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 st.title('Uber pickups in USA')
 st.title(random.randint(1,100))
 
-number1 = st.number_input('Insert a number')
-number2 = st.number_input('Insert a number')
+jozo = st.number_input('Insert a number')
+fero = st.number_input('Insert a number')
 
 # st.write('The current number is ', number1 + number2)
